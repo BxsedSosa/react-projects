8b9d870ce36f49a837d6223d3ef86843cbee0bf0
BxsedSosa <donovanxabad@gmail.com>
2025-01-06 13:54:08 -0800

    starting project

---
+++ todo-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function MyButton() {
+  return <button>Hello</button>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is my beginning of a project</h1>
+        <MyButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;

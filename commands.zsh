691c6d13c83fb24c8a74edf62b2121174fcc0a2d
BxsedSosa <donovanxabad@gmail.com>
2025-01-06 14:45:04 -0800

    Created a zsh file to auto input commands to clean up the react issues when starting a new project

---
+++ commands.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+npm uninstall react react-dom
+
+npm install react@18 react-dom@18
+
+npm install --no-audit --save @testing-library/jest-dom@^5.14.1 @testing-library/react@^13.0.0 @testing-library/user-event@^13.2.1 web-vitals@^2.1.0
+
+npm install --force
+

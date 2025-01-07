a123d5bd9e91510ccf8b20bd0d555446636aa7b6
BxsedSosa <donovanxabad@protonmail.com>
2025-01-06 18:10:35 -0800

    fixed file for wsl

--- commands.zsh
+++ commands.zsh
@@ -1,4 +1,3 @@
-#!/bin/zsh
 
 npm uninstall react react-dom
 
@@ -7,4 +6,3 @@ npm install react@18 react-dom@18
 npm install --no-audit --save @testing-library/jest-dom@^5.14.1 @testing-library/react@^13.0.0 @testing-library/user-event@^13.2.1 web-vitals@^2.1.0
 
 npm install --force
-
